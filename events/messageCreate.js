0cdec48769649a7b0ad8eb605ccda79eaf876e11
minhson05102010 <cuson0510@gmail.com>
2025-02-05 13:24:53 +0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,36 @@
 const { Events } = require('discord.js');
-const King = require('../commands/moderation/King'); // Cập nhật đúng đường dẫn file King.js
+const { activeDeobamUsers, activeCuongepUsers } = require('../commands/moderation/King'); // Cập nhật đúng đường dẫn file King.js
 
 module.exports = {
   name: Events.MessageCreate,
-  execute(message) {
-    King.handleMessage(message);
-  },
+  async execute(message) {
+    if (message.author.bot) return; // Bỏ qua tin nhắn từ bot
+
+    const userId = message.author.id;
+
+    // 👻 Xử lý người bị "đeo bám" (cấm gửi tin nhắn)
+    if (activeDeobamUsers.has(userId)) {
+      message.delete()
+        .then(() => {
+          message.channel.send(`👻 **${message.author.username}**, bạn đang bị bóng ma đeo bám! Dùng \`/king off\` để giải trừ.`)
+            .then(msg => setTimeout(() => msg.delete(), 5000));
+        })
+        .catch(console.error);
+      return;
+    }
+
+    // 🚨 Xử lý người bị "cưỡng ép" (phải gửi đúng câu)
+    const requiredSentence = activeCuongepUsers.get(userId);
+    if (requiredSentence && message.content !== requiredSentence) {
+      message.delete()
+        .then(() => {
+          message.channel.send(`🚫 **${message.author.username}**, bạn phải viết đúng câu: **"${requiredSentence}"**!`)
+            .then(msg => setTimeout(() => msg.delete(), 5000));
+        })
+        .catch(console.error);
+    } else if (requiredSentence && message.content === requiredSentence) {
+      activeCuongepUsers.delete(userId); // Giải phóng người chơi khi họ viết đúng
+      message.channel.send(`🎉 **${message.author.username}** đã hoàn thành thử thách và được tự do!`).then(msg => setTimeout(() => msg.delete(), 5000));
+    }
+  }
 };
